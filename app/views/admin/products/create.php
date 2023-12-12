5650f6e73b395995903df9c09fba956b5f0f9fc3
Si Tran Van <0982093650a@gmail.com>
2023-12-13 02:53:47 +0700

    xong bug ID=8

--- app/views/admin/products/create.php
+++ app/views/admin/products/create.php
@@ -5,7 +5,7 @@
         <div class="col-lg-6 col-md-12 d-flex flex-column gap-3">
             <div class="form-group">
                 <label class="form-label" for="">Tên sản phẩm</label>
-                <input value="<?= old($old, 'name') ?>" name="name" type="text" class="form-control <?= isInvalid($errors, 'name') ?>">
+                <input value="<?= old($old, 'name')  ?>" name="name" type="text" class="form-control <?= isInvalid($errors, 'name') ?>">
                 <div class="invalid-feedback"><?= getMessageError($errors, 'name') ?></div>
             </div>
             <div class="form-group">
@@ -72,7 +72,6 @@
     </div>
     <div class="mt-4">
         <button class="btn btn-primary px-4">Thêm sản phẩm</button>
-
     </div>
 </form>
 
--- app/views/admin/products/edit.php
+++ app/views/admin/products/edit.php
@@ -5,24 +5,31 @@
         <div class="col-lg-6 col-md-12 d-flex flex-column gap-3">
             <div class="form-group">
                 <label class="form-label" for="">Tên sản phẩm</label>
-                <input value="<?= $product['name'] ?? '' ?>" name="name" type="text" class="form-control">
+                <input value="<?= $product['name'] ?? old($old, 'name') ?>" name="name" type="text" class="form-control <?= isInvalid($errors, 'name') ?>">
+                <div class="invalid-feedback"><?= getMessageError($errors, 'name') ?></div>
             </div>
             <div class="form-group">
                 <label class="form-label" for="">Giá gốc</label>
-                <input value="<?= $product['price'] ?? '' ?>" name="price" type="text" class="form-control">
+                <input value="<?= $product['price'] ?? old($old, 'price')  ?>" name="price" type="text" class="form-control <?= isInvalid($errors, 'price') ?>">
+                <div class="invalid-feedback"><?= getMessageError($errors, 'price') ?></div>
+
             </div>
             <div class="form-group">
                 <label class="form-label" for="">Giá khuyến mãi</label>
-                <input value="<?= $product['price_promotion'] ?? '' ?>" name="price_promotion" type="text" class="form-control">
+                <input value="<?= $product['price_promotion'] ?? old($old, 'price_promotion') ?>" name="price_promotion" type="text" class="form-control">
             </div>
             <div class="form-group">
                 <label class="form-label" for="">Hình ảnh</label>
                 <div class="custom-file-input">
-                    <input name="photo" type="file" id="fileInput" class="input-file" accept="image/*">
+                    <input name="photo" type="file" id="fileInput" class="input-file form-control <?= isInvalid($errors, 'photo') ?>" accept="image/*">
                     <label for="fileInput" id="fileInputLabel">
                         <span id="fileSelectedText">Choose a file</span>
-                        <img id="selectedImage" src="#" alt="Selected Image">
+                        <img width="150" id="selectedImage" src="" alt="Selected Image">
+                        <?php if (!empty($product['photo'])) : ?>
+                            <img id="review-edit" width="150" src="<?= getImage($product['photo']) ?>" alt="<?= $product['name'] ?>">
+                        <?php endif ?>
                     </label>
+                    <div class="invalid-feedback"><?= getMessageError($errors, 'photo') ?></div>
                 </div>
             </div>
         </div>
@@ -31,7 +38,7 @@
                 <label class="form-label" for="categories">Danh mục</label>
                 <select class="form-select" name="category_id" id="">
                     <?php foreach ($allCategory as $key => $category) : ?>
-                        <option <?= $product['category_id'] == $category['id'] ? 'selected' : false ?> value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+                        <option <?= ($product['category_id'] == $category['id']) || (isset($old) && $old['category_id'] == $category['id']) ? 'selected' : false ?> value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
                     <?php endforeach ?>
                 </select>
             </div>
@@ -39,15 +46,15 @@
                 <label class="form-label" for="brands">Thương hiệu</label>
                 <select class="form-select" name="brand_id" id="">
                     <?php foreach ($allBrand as $key => $brand) : ?>
-                        <option <?= $product['brand_id'] == $brand['id'] ? 'selected' : false ?> value="<?= $brand['id'] ?>"><?= $brand['name'] ?></option>
+                        <option <?= ($product['brand_id'] == $brand['id']) || (isset($old) && $old['brand_id'] == $brand['id']) ? 'selected' : false ?> value="<?= $brand['id'] ?>"><?= $brand['name'] ?></option>
                     <?php endforeach ?>
                 </select>
             </div>
             <div class="form-group">
                 <label class="form-label" for="Danh mục">Trạng thái</label>
                 <select class="form-select" name="active" id="">
-                    <option <?= $product['active'] == 0 ? 'selected' : false ?> value="0">Chưa kích hoạt</option>
-                    <option <?= $product['active'] == 1 ? 'selected' : false ?> value="1">Kích hoạt</option>
+                    <option <?= ($product['active'] == 0 || isset($old) && $old['active'] == 0) ? 'selected' : false ?> value="0">Chưa kích hoạt</option>
+                    <option <?= ($product['active'] == 1 || isset($old) && $old['active'] == 1) ? 'selected' : false ?> value="1">Đã kích hoạt</option>
                 </select>
             </div>
         </div>
